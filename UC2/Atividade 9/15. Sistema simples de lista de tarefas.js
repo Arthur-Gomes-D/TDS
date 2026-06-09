cc879d21940fa58a451373b24dd34e4ac3f7abdd
Arthur-gomes-D <arthurgomesdevantier@gmail.com>
2026-06-09 13:57:38 -0300

    tarefa 15 da atividade 9 completada

---
+++ UC2/Atividade 9/15. Sistema simples de lista de tarefas.js
@@ -0,0 +1,10 @@
+
+
+let tarefas = []
+
+tarefas.push("Estudar JavaScript", "Fazer exercícios", "Revisar arrays")
+console.log(tarefas);
+console.log(tarefas[tarefas.length-1]);
+
+tarefas.pop()
+console.log(tarefas);
