6a29baf862a966a230328ffd1c738921184806ec
Arthur-gomes-D <arthurgomesdevantier@gmail.com>
2026-07-17 17:15:57 -0300

    fim aula 1707

---
+++ UC3/Atividade 7/Exercício 1.sql
@@ -0,0 +1,34 @@
+-- 1
+
+CREATE DATABASE crm_vendas;
+USE crm_vendas;
+
+-- 2
+
+CREATE TABLE leads(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    telefone VARCHAR(20) UNIQUE,
+    empresa VARCHAR(100) NOT NULL,
+    data_cadastro DATE NOT NULL
+);
+
+CREATE TABLE interacoes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(100) NOT NULL,
+    data_intecao DATETIME NOT NULL,
+    tipo VARCHAR(45),
+    id_leads INT,
+    FOREIGN KEY (id_leads) REFERENCES leads(id) ON DELETE CASCADE
+);
+
+CREATE TABLE oportunidades(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_interesse VARCHAR(45) NOT NULL,
+    valor_previsto DECIMAL(10,2) NOT NULL,
+    status INT NOT NULL,
+    data_criacao DATE NOT NULL,
+    id_leads INT,
+    FOREIGN KEY (id_leads) REFERENCES leads(id) ON DELETE CASCADE
+);
