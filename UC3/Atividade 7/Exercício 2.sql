562d750c2dee2a0af80010e75b112dbf9368d437
Arthur-gomes-D <arthurgomesdevantier@gmail.com>
2026-07-20 16:20:56 -0300

    tabela lojas criada

--- UC3/Atividade 7/Exercício 2.sql
+++ UC3/Atividade 7/Exercício 2.sql
@@ -1,3 +1,9 @@
 -- 1
 CREATE DATABASE monitor_precos;
 USE monitor_precos;
+--2
+CREATE TABLE lojas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_loja VARCHAR(100) NOT NULL,
+    site VARCHAR(150)
+);
