7883f4f3c7a0f9b86a774b92422aee92b4ae9cac
Arthur-Gomes-D <arthurgomesdevantier@gmail.com>
2026-08-17 14:49:03 -0300

    exercicio 10

---
+++ UC4/Atividade 4/src/Exercício 10 Cofre Digital com Tentativas.ts
@@ -0,0 +1,38 @@
+class Cofre {
+    private segredo: string
+    private aberto: boolean
+    private tentativasFalhas: number
+    constructor() {
+        this.segredo = "1a2b3c"
+        this.aberto = false
+        this.tentativasFalhas = 0
+    }
+    public abrir(segredoDigitado: string): void {
+        if (this.tentativasFalhas < 3) {
+            if (segredoDigitado == this.segredo) {
+                this.aberto = true
+            } else {
+                this.tentativasFalhas += 1
+                console.log(this.tentativasFalhas);
+                
+            }
+        }else{
+            throw new Error("maxima atingida");
+            
+        }
+        if (this.aberto) {
+            console.log("cofre aberto");
+        }
+    }
+}
+let cof = new Cofre()
+
+cof.abrir("dsadfas")
+cof.abrir("dsadfas")
+cof.abrir("1a2b3c")
+try {
+    cof.abrir("dsadfas")
+} catch (error) {
+    console.log("tentativa maxima atingida");
+    
+}
