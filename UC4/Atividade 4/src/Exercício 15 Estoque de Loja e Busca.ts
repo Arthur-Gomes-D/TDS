be3c79eb10ec1f42bfe1b63abdf1c75562e2bf73
Arthur-Gomes-D <arthurgomesdevantier@gmail.com>
2026-08-18 22:50:08 -0300

    exercicio 15 terminado

--- UC4/Atividade 4/src/Exercício 15 Estoque de Loja e Busca.ts
+++ UC4/Atividade 4/src/Exercício 15 Estoque de Loja e Busca.ts
@@ -1,11 +1,33 @@
+interface Produtos{
+    id:number,
+    nome:string,
+    qtd:number
+}
 class Estoque {
-    private produtos:Object[]
+    private produtos:Produtos[]
     constructor(){
         this.produtos = []
     }
     public adicionarProduto(id:number, nome: string, qtd: number):void{
+        let idRepetido:boolean = false
         if (this.produtos.find((produto) => produto.id == id)) {
-            
+            idRepetido = true
+            console.log("já existe um produto com esse id");
+        }
+        if (!idRepetido) {
+            this.produtos.push({id:id, nome:nome, qtd:qtd})
+        }
+    }
+    public atualizarQuantidade(id:number, novoQtd:number):void{
+        for (const produto of this.produtos) {
+            if (produto.id == id) {
+                produto.qtd = novoQtd
+                console.log(`quantidade de produto de ${id} foi atualizado para ${novoQtd}`);
+            }
         }
     }
 }
+let meuEstoque = new Estoque
+meuEstoque.adicionarProduto(1, "maçã", 5)
+meuEstoque.adicionarProduto(1, "maçã", 5)
+meuEstoque.atualizarQuantidade(1, 50)
